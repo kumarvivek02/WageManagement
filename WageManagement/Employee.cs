50e9f88b0bf13f56cb0958d0d15530e67f7d524a
Vivek Kumar <kumarvivek02@gmail.com>
2017-08-29 23:48:26 -0500

    Initial Commit

---
+++ WageManagement/Employee.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WageManagement
+{
+    class Employee
+    {
+        //Constructor
+
+        //Fields
+        private string fName;
+
+        public string FirstName
+        {
+            get { return fName; }
+            set { fName = value; }
+        }
+
+        private string lName;
+
+        public string LastName
+        {
+            get { return lName; }
+            set { lName = value; }
+        }
+
+
+        private int empLevel;
+
+        public int EmployeeLevel
+        {
+            get { return empLevel; }
+            set { empLevel = value; }
+        }
+        
+
+
+        //Methods
+
+        
+    }
+}
---
+++ WageManagement/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Office.Interop.Excel;
+
+namespace WageManagement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            ObservableCollection<Employee> employeeList = new ObservableCollection<Employee>();
+
+           //Application xlApp = new Application();
+
+           //if (xlApp == null)
+           //{
+           //    Console.WriteLine("EXCEL could not be started. Check that your office installation and project references are correct.");
+           //    return;
+           //}
+           //xlApp.Visible = true;
+           // xlApp.Workbooks.Open("..\\EmployeeAttendance.xls");
+
+
+            Application exlApp= new Application() ;
+            Workbook xlWorkBook = exlApp.Workbooks.Open(@"d:\EmployeeAttendance.xlsx", 0, true, 5, "", "", true, Microsoft.Office.Interop.Excel.XlPlatform.xlWindows, "\t", false, false, 0, true, 1, 0); ;
+            Worksheet xlWorkSheet = xlWorkBook.Worksheets.get_Item(1); 
+
+
+        }
+    }
+}
